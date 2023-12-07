042863c39100d199150605691e112433d3f24a37
Brooklyn Mcswiney <brooklynmcswiney@gmail.com>
2023-12-07 17:44:07 +0000

    finished editing questions

--- code/graph_gen/createGraphs.py
+++ code/graph_gen/createGraphs.py
@@ -3,7 +3,7 @@ from graphGen import gen_line, gen_area
 def generate_graphs():
     country_list = ["USA", "Great Britain", "Australia", "Japan", "Germany"]
     country_list_short = ["Great Britain", "Australia", "Japan", "Germany"]
-    years_list = ["Year 1", "Year 2", "Year 3", "Year 4", "Year 5"]
+    years_list = ["Year 1", "Year 2", "Year 3"]
 
     # x_labels, gold_medals, silver_medals, bronze_medals, graph_label, trial, graph_numb
     # Question 1 and 2
@@ -71,7 +71,7 @@ def generate_graphs():
         country_list,
         [45, 20, 8, 7, 13],
         [31, 23, 14, 9, 15],
-        [30, 12, 10, 10, 14],
+        [30, 18, 10, 10, 14],
         "A - ",
         "max",
         3
@@ -80,27 +80,28 @@ def generate_graphs():
         country_list,
         [37, 16, 14, 7, 15],
         [22, 20, 15, 13, 13],
-        [37, 13, 16, 19, 13],
+        [37, 7, 16, 19, 13],
         "B - ",
         "max",
         4
     )
 
-    # Question 9 and 10
+    # Question 9
     gen_line(
         years_list,
-        [38, 48, 45, 46, 43],
-        [33, 24, 34, 22, 30],
-        [37, 36, 31, 36, 37],
+        [38, 48, 46 -7],
+        [33, 24, 22 - 4],
+        [37, 36, 36 - 10 ],
         "B - ",
         "years",
         2
     )
+    # Question 10
     gen_line(
         years_list,
-        [32, 48, 45, 46, 43],
-        [36, 24, 34, 22, 30],
-        [44, 36, 31, 36, 37],
+        [32, 48 + 5, 45],
+        [36, 24 + 10, 34],
+        [44, 36 + 10, 31],
         "A - ",
         "years",
         1
@@ -109,18 +110,18 @@ def generate_graphs():
     # Question 11 and 12
     gen_area(
         years_list,
-        [45, 42, 45, 38, 38],
-        [29, 25, 35, 24, 24],
-        [38, 35, 34, 31, 33],
+        [45, 41, 39],
+        [29, 25, 25],
+        [38, 35, 34],
         "A - ",
         "years",
         1
     )
     gen_area(
         years_list,
-        [46, 36, 41, 41, 37],
-        [29, 22, 30, 24, 23],
-        [37, 35, 33, 37, 38],
+        [46, 36, 41],
+        [29, 22, 30],
+        [37, 35, 33],
         "B - ",
         "years",
         2
@@ -129,18 +130,18 @@ def generate_graphs():
     # Question 13
     gen_line(
         years_list,
-        [32, 38, 35, 36, 47],
-        [31, 39, 32, 36, 37],
-        [33, 24, 34, 22, 30],
+        [32, 38, 35],
+        [31, 39, 32],
+        [33, 24, 34],
         "A - ",
         "years",
         3
     )
     gen_line(
         years_list,
-        [44, 36, 31, 36, 37],
-        [32, 38, 35, 46, 43],
-        [36, 24, 34, 22, 30],
+        [44, 36, 31],
+        [32, 38, 35],
+        [36, 24, 34],
         "B - ",
         "years",
         4
@@ -149,18 +150,18 @@ def generate_graphs():
     # Question 14
     gen_area(
         years_list,
-        [32, 38, 35, 36, 47],
-        [31, 39, 32, 36, 37],
-        [33, 24, 34, 22, 30],
+        [32, 38, 35],
+        [31, 39, 32],
+        [33, 24, 34],
         "A - ",
         "years",
         3
     )
     gen_area(
         years_list,
-        [44, 36, 31, 36, 37],
-        [32, 38, 35, 46, 43],
-        [36, 24, 34, 22, 30],
+        [44, 36, 31],
+        [32, 38, 35],
+        [36, 24, 34],
         "B - ",
         "years",
         4
@@ -178,9 +179,9 @@ def generate_graphs():
     )
     gen_line(
         country_list_short,
-        [14, 8, 7, 13],
-        [17, 8, 8, 5],
-        [9, 10, 9, 10],
+        [14, 18, 7, 13],
+        [17, 17, 8, 5],
+        [7, 9, 8, 10],
         "",
         "country",
         2
@@ -189,7 +190,7 @@ def generate_graphs():
     # Question 17 and 18
     gen_area(
         country_list_short,
-        [16, 12, 7, 13],
+        [16, 12, 14, 18],
         [14, 15, 11, 14],
         [12, 15, 13, 13],
         "",
@@ -199,14 +200,34 @@ def generate_graphs():
     gen_area(
         country_list_short,
         [18, 19, 16, 20],
-        [12, 22, 14, 19],
-        [10, 13, 18, 8],
+        [12, 28, 14, 19],
+        [6, 13, 18, 8],
         "",
         "country",
         2
     )
 
-    # NOTE: Question 19 and 20 will compare pre-existing graphs
+    # Question 19
+    gen_area(
+        country_list,
+        [39, 8, 3, 7, 9],
+        [32, 4, 12, 6, 14],
+        [32, 11, 6, 12, 8],
+        "",
+        "same",
+        1
+    )
+    gen_line(
+        country_list,
+        [39, 8, 3, 7, 9],
+        [32, 4, 12, 6, 14],
+        [32, 11, 6, 12, 8],
+        "",
+        "same",
+        1
+    )
+
+    # NOTE: Question 20 will compare pre-existing graphs
 
 
 # gen_line(
--- code/main.py
+++ code/main.py
@@ -8,15 +8,15 @@ question = Question("Which Graph shows more gold medals for GB?",
                     "assets/line-max-trial-2.png", "A", ["A", "", "", "","B"], "line")
 question2 = Question("Which Graph shows more total medals for the USA?",
                     "assets/line-max-trial-1.png",
-                    "assets/line-max-trial-2.png", "B", ["A", "", "", "", "B"], "line")
+                    "assets/line-max-trial-2.png", "A", ["A", "", "", "", "B"], "line")
 
 # Area max trial 1 and 2
 question3 = Question("Which Graph shows more gold medals for GB?",
                     "assets/area-max-trial-1.png",
-                    "assets/area-max-trial-2.png", "B", ["A", "", "", "", "B"], "area")
+                    "assets/area-max-trial-2.png", "A", ["A", "", "", "", "B"], "area")
 question4 = Question("Which Graph shows more total medals for the USA?",
                     "assets/area-max-trial-1.png",
-                    "assets/area-max-trial-2.png", "B", ["A", "", "", "", "B"], "area")
+                    "assets/area-max-trial-2.png", "A", ["A", "", "", "", "B"], "area")
 
 # Line max trial 3 and 4
 question5 = Question("In which Graph did GB win less bronze medals?",
@@ -24,68 +24,68 @@ question5 = Question("In which Graph did GB win less bronze medals?",
                     "assets/line-max-trial-4.png", "B", ["A", "", "", "", "B"], "line")
 question6 = Question("What is the sum of the silver medals won by Germany in both graphs?",
                     "assets/line-max-trial-3.png",
-                    "assets/line-max-trial-4.png", "44", ["38", "40", "44", "46", "48"], "line")
+                    "assets/line-max-trial-4.png", "25", ["13", "25", "31", "35", "38"], "line")
 
 # Area max trial 3 and 4
 question7 = Question("In which Graph did GB win less bronze medals?",
                     "assets/area-max-trial-3.png",
-                    "assets/area-max-trial-4.png", "A", ["A", "", "", "", "B"], "area")
+                    "assets/area-max-trial-4.png", "B", ["A", "", "", "", "B"], "area")
 question8 = Question("What is the sum of the silver medals won by Germany in both graphs?",
                     "assets/area-max-trial-3.png",
-                    "assets/area-max-trial-4.png", "37", ["33", "35", "37", "39", "41"], "area")
+                    "assets/area-max-trial-4.png", "31", ["29", "23", "27", "31", "35"], "area")
 
 # line years trial 1 and 2
-question9 = Question("In Graph A, which year shows the least amount of total medals gained?",
-                    "assets/line-years-trial-1.png", # 109, 111, 112, 
-                    "assets/line-years-trial-2.png", "Year 1", ["Year 1", "Year 2", "Year 3", "Year 4", "Year 5"], "line")
-question10 = Question("In Graph B, which year shows the least amount of total medals gained??",
-                    "assets/line-years-trial-1.png",
-                    "assets/line-years-trial-2.png", "Year 2", ["Year 1", "Year 2", "Year 3", "Year 4", "Year 5"], "line")
+question9 = Question("which year shows the least amount of total medals gained?",
+                    "assets/line-years-trial-1.png", # Graph 2 shown
+                    "assets/line-years-trial-2.png", "Year 3", ["Year 1", "Year 2", "Year 3", "", ""], "line")
+question10 = Question("Which year shows the most amount of total medals gained??",
+                    "assets/line-years-trial-1.png", # Graph 1 shown
+                    "assets/line-years-trial-2.png", "Year 2", ["Year 1", "Year 2", "Year 3", "", ""], "line")
 
 # area years trial 1 and 2
-question11 = Question("In Graph A, which year shows the least amount of total medals gained?",
-                    "assets/area-years-trial-1.png",
-                    "assets/area-years-trial-2.png", "Year 3", ["Year 1", "Year 2", "Year 3", "Year 4", "Year 5"], "area")
-question12 = Question("In Graph B, which year shows the least amount of total medals gained?",
-                    "assets/area-years-trial-1.png",
-                    "assets/area-years-trial-2.png", "Year 4", ["Year 1", "Year 2", "Year 3", "Year 4", "Year 5"], "area")
+question11 = Question("Which year shows the least amount of total medals gained?",
+                    "assets/area-years-trial-1.png", # Graph 2 shown
+                    "assets/area-years-trial-2.png", "Year 2", ["Year 1", "Year 2", "Year 3", "", ""], "area")
+question12 = Question("Which year shows the most amount of total medals gained?",
+                    "assets/area-years-trial-1.png", # Graph 1 shown
+                    "assets/area-years-trial-2.png", "Year 1", ["Year 1", "Year 2", "Year 3", "", ""], "area")
 
 # line years trial 3 and 4
-question13 = Question("In Graph A, which country got the most medals?",
-                    "assets/line-years-trial-3.png",
-                    "assets/line-years-trial-4.png", "GB", ["GB", "AUS", "JP", "GER", ""], "line")
+question13 = Question("Which year shows the most gold medals?",
+                    "assets/line-years-trial-3.png", # Graph 2 shown
+                    "assets/line-years-trial-4.png", "Year 1", ["Year 1", "Year 2", "Year 3", "", ""], "line")
 # area years trial 3 and 4
-question14 = Question("In Graph A, which country got the 2nd highest amount of medals?",
-                    "assets/area-years-trial-3.png",
-                    "assets/area-years-trial-4.png", "GER", ["GB", "AUS", "JP", "GER", ""], "line")
+question14 = Question("Which year shows the most gold medals?",
+                    "assets/area-years-trial-3.png", # Graph 2 shown
+                    "assets/area-years-trial-4.png", "Year 1", ["Year 1", "Year 2", "Year 3", "", ""], "area")
 
 # line country trial 1 and 2
-question15 = Question("In Graph B, which country got the most medals?",
-                    "assets/line-country-trial-1.png",
-                    "assets/line-country-trial-2.png", "GB", ["GB", "AUS", "JP", "GER", ""], "line")
-question16 = Question("In Graph B, which country got the 2nd highest amount of medals?",
-                    "assets/line-max-trial-5.png",
-                    "assets/line-max-trial-6.png", "AUS", ["GB", "AUS", "JP", "GER", ""], "line")
+question15 = Question("Which country got the most medals?",
+                    "assets/line-country-trial-1.png", # Graph 1 shown
+                    "assets/line-country-trial-2.png", "AUS", ["GB", "AUS", "JP", "GER", ""], "line")
+question16 = Question("Which country got the 2nd highest amount of silver medals?",
+                    "assets/line-country-trial-1.png", # Graph 1 shown
+                    "assets/line-country-trial-2.png", "JP", ["GB", "AUS", "JP", "GER", ""], "line")
 
 # area country trial 1 and 2
 question17 = Question("In Graph A, which country got the most medals?",
-                    "assets/area-max-trial-5.png",
-                    "assets/area-max-trial-6.png", "GER", ["GB", "AUS", "JP", "GER", ""], "area")
-question18 = Question("In Graph A, which country got the 2nd highest amount of medals?",
-                    "assets/area-max-trial-5.png",
-                    "assets/area-max-trial-6.png", "AUS", ["GB", "AUS", "JP", "GER", ""], "area")
+                    "assets/area-country-trial-1.png", # Graph 1 shown
+                    "assets/area-country-trial-2.png", "GER", ["GB", "AUS", "JP", "GER", ""], "area")
+question18 = Question("In Graph B, which country got the 2nd highest amount of silver medals?",
+                    "assets/area-country-trial-1.png", # graph 2 shown
+                    "assets/area-country-trial-2.png", "GER", ["GB", "AUS", "JP", "GER", ""], "area")
 
 # Comparison trials
-question19 = Question("In Graph B, which country got the most medals?",
-                    "assets/area-max-trial-5.png",
-                    "assets/area-max-trial-6.png", "GER", ["GB", "AUS", "JP", "GER", ""], "area")
-question20 = Question("In Graph B, which country got the 2nd highest amount of medals?",
-                    "assets/area-max-trial-5.png",
-                    "assets/area-max-trial-6.png", "GB", ["GB", "AUS", "JP", "GER", ""], "area")
+question19 = Question("Do these graphs have the same datasets?",
+                    "assets/line-same-trial-1.png",
+                    "assets/area-same-trial-1.png", "Yes", ["Yes", "", "", "", "No"], "mix")
+question20 = Question("Do these graphs have the same datasets?",
+                    "assets/line-max-trial-1.png",
+                    "assets/area-max-trial-1.png", "No", ["Yes", "", "", "", "No"], "mix")
 
 question21 = Question("Which graphtype do you find easier to read??",
-                    "assets/line-max-trial-1.png",
-                    "assets/area-max-trial-1.png", "line", ["line", "", "", "", "area"], "")
+                    "assets/line-max-trial-2.png",
+                    "assets/area-max-trial-2.png", "", ["line", "", "", "", "area"], "N/A")
 
 
 question_list = []
